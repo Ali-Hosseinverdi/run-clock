3920e28dbf4c710325dafb91640889cce17222bc
Ali Hosseinverdi <alihv5000@gmail.com>
2022-06-27 00:05:16 -0700

    change input type from string to integer

--- runclock/runclock.py
+++ runclock/runclock.py
@@ -1,20 +1,19 @@
-from time import sleep
-from time import ctime
+from time import sleep, ctime
 
-def run_at(hour:str, minute:str = '00', second:str = '00') -> None:
-    if type(hour) != str or type(minute) != str or type(second) != str:
-        raise TypeError('Function takes string but integer given')
+def run_at(hour:int, minute:int = 0, second:int = 0) -> None:
+    if type(hour) != int or type(minute) != int or type(second) != int:
+        raise TypeError('Function takes integer but string given')
 
-    hour_n = 0
-    minute_n = 0
-    second_n = 0
+    hour_n = -1
+    minute_n = -1
+    second_n = -1
 
     while hour != hour_n or minute != minute_n or second != second_n:
         time_now = ctime().split(' ')
         clock_now = time_now[3].split(':')
 
-        hour_n = clock_now[0]
-        minute_n = clock_now[1]
-        second_n = clock_now[2]
+        hour_n = int(clock_now[0])
+        minute_n = int(clock_now[1])
+        second_n = int(clock_now[2])
 
         sleep(1)
